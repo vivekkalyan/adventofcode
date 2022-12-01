d566926dd714fd4cc3008a4a1002ca8266b32c03
Vivek Kalyan <hello@vivekkalyan.com>
2022-12-01 17:05:05 +0800

    Add day 01 solutions

---
+++ 2022/day01/src/main.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input").expect("Should be able to read file");
+    let mut calories: Vec<i32> = contents
+        .split("\n\n")
+        .map(|s| {
+            s.split("\n")
+                .map(|i| i.parse::<i32>().unwrap_or_else(|_| 0))
+                .sum::<i32>()
+        })
+        .collect();
+    calories.sort();
+    calories.reverse();
+    println!("The elf carrying the most number of calories has is carrying {:?} calories", calories[0]);
+    println!("The elves carrying the 3 most number of calories has is carrying a total of {:?} calories", calories[0] + calories[1] + calories[2]);
+}
